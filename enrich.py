236271753345e1cbfa9183f6923beca7e032edb4
Patrick Glynn <p.glynn18@gmail.com>
2026-04-02 15:08:29 +0100

    added logic to make request get concurrent and threaded and execute faster

--- enrich.py
+++ enrich.py
@@ -26,13 +26,15 @@ small_df = pl.read_csv('members/td_tables.csv')
 large_df = pl.read_csv('members/flattened_members.csv')
 
 small_df = normalise_join_key.normalise_df_td_name(small_df)
-print('normalised small_df TD names')
+print('normalised small_df (PDF attendance) TD names')
 large_df = normalise_join_key.normalise_df_td_name(large_df)
-print('normalised small_df TD names')
+large_df = large_df.unique(subset=['join_key'], keep='first')
+print('normalised large_df (API members) TD names')
 
 # https://regex101.com/r/OOLuZU/1
+# API master list is the driving table; PDF attendance is left-joined onto it
 large_df = large_df.select(cols_to_select)
-enriched_df = small_df.join(large_df, on=['join_key'], how='left')
+enriched_df = large_df.join(small_df, on=['join_key'], how='left')
 enriched_df = enriched_df.with_columns(pl.col('unique_member_code').str.extract(r"\b\d{4}\b", 0).alias('year_elected')
                                        )
 enriched_df= enriched_df.with_columns(
