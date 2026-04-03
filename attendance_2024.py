1e3a2a58cf59e39cb98a313f6a748a81ece97987
Patrick Glynn <YOUR_ID+peweet@users.noreply.github.com>
2026-04-03 15:50:27 +0100

    made more flexible and adaptable the central api service and added more logging across the project

--- attendance_2024.py
+++ attendance_2024.py
@@ -8,7 +8,7 @@ import pandas as pd
 from numpy import nan
 import os                                                                   
 import glob    
-
+import logger
 # ---Attendance Dail Eireann 2023, 2024, 2025 attendance PDF ---
 dataframes = []
 IRISH_NAME_REGEX = re.compile(r"^[A-ZÁÉÍÓÚ][a-zA-ZáéíóúÁÉÍÓÚ'\s\-]+$")
@@ -55,8 +55,8 @@ for pdf in list(glob.glob('*.pdf')):
     )   # Add a column counting non-empty attendance entries
     df = pd.merge(df, result, on='identifier', how='left') # Join the counts back to the original DataFrame
     df = df.drop('identifier', axis=1) # Drop the identifier column as it's no longer needed
-    print('Final DataFrame with attendance counts:')
+    logging.info('Final DataFrame with attendance counts:')
     df['sitting_total_days'] = df['sitting_days_count'] + df['other_days_count']
     df.to_csv('C:\\Users\\pglyn\\PycharmProjects\\dail_extractor\\members\\aggregated_td_tables.csv', index=False) 
-    print("TD attendance CSV created successfully.")
+    logging.info("TD attendance CSV created successfully.")
 
