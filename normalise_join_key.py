e9a841c0136ce9fe231fe7a495927cf8713a8e4c
Patrick Glynn <p.glynn18@gmail.com>
2026-04-07 12:44:14 +0100

    added logic around pulling payment data from available pdfs

--- normalise_join_key.py
+++ normalise_join_key.py
@@ -1,7 +1,8 @@
 import polars as pl
 import pandas as pd
+import logging
 # # https://docs.python.org/3/library/unicodedata.html#unicodedata.normalize
-def normalise_df_td_name(df : pl.DataFrame ) -> pl.DataFrame:
+def normalise_df_td_name(df : pl.DataFrame | pl.Series) -> pl.DataFrame| pl.Series:
     docstring = """
     Normalises TD names in the given DataFrame by:
     1. Concatenating first and last names into a single 'join_key' column.
@@ -15,30 +16,35 @@ def normalise_df_td_name(df : pl.DataFrame ) -> pl.DataFrame:
     TD names across different datasets, even if there are variations in spelling, formatting, or special characters.
     It is an intermediate step in the data enrichment pipeline, used to join attendance records with member metadata.
     """
-    print('normalising names in dataframe...')
+    logging.info('Normalising names in dataframe...')
     if type(df) == pd.DataFrame:
-       print('Pandas dataframe detected, converting to Polars DataFrame for normalization.')
+       logging.info('Pandas dataframe detected, converting to Polars DataFrame for normalization.')
        df = pl.from_pandas(df)
-    if 'first_name' not in df.columns or 'last_name' not in df.columns:
+    elif 'first_name' not in df.columns or 'last_name' not in df.columns:
         raise ValueError("DataFrame must contain 'first_name' and 'last_name' columns for normalization.")   
+    elif df is None or df.is_empty():
+        logging.warning('Input DataFrame is empty or None. Returning empty DataFrame.')
+        return pl.DataFrame()  # Return an empty DataFrame if input is empty or None
+    elif type(df) == pl.Series:
+        logging.info('Polars Series detected, converting to DataFrame for normalization.')
+        df = df.to_frame()
     # both_names = dataframe.select({'first_name', 'last_name'})
     both_names = df.with_columns(
         pl.concat_str(
         pl.col(['first_name', 'last_name'])
-        ).alias('join_key').str.to_lowercase()
+        ).alias('join_key')
         )
     full_name = both_names.with_columns(
-        # NFD converts accented chars into base letter + combining accent mark
-        # e.g. á → a + U+0301, é → e + U+0301, ó → o + U+0301
-        pl.col('join_key').str.normalize("NFD")
-        # [\u0300-\u036f] is the Unicode "Combining Diacritical Marks" block.
-        # After NFD, accents become separate code points in this range.
-        # Stripping them converts á→a, é→e, ó→o etc. while keeping the base letter.
-        .str.replace_all(r"[\u0300-\u036f]", "")
-        .str.replace_all(r"[^a-z\s]", "").alias('join_key')
+        # NFD converts accented chars into base letter + combining accent mark to make it easier to join
+        pl.col('join_key')
+        .str.to_lowercase()
+        .str.replace_all(r"[\x27\u2019]", "") #remove apostrophes as they cause too many issues with joining names across datasets (e.g. O'Sullivan becomes OSullivan, which is easier to match with the same name in another dataset that doesn't include the apostrophe)
+        .str.replace_all(r"[\u0300-\u036f]", "") # remove accents and fadas as it becomes too difficult to join names with special characters otherwise (e.g. O'Sullivan, Ó Súilleabháin)
+        .str.replace_all(r"[^a-z\s]", "")
+        .str.normalize("NFD").alias('join_key')
         .str.replace_all(r"\s+", "")# Remove all whitespace
         .str.extract_all(r".")    # Extract individual characters into a list       
         .list.sort() # Sort the list of characters alphabetically (e.g. "OSuilleabhain" → "Oabhiillnsuu")
         .list.join("") # Join the sorted characters back into a string
-        )   
+        )
     return full_name
--- payments.py
+++ payments.py
@@ -1,31 +1,55 @@
 import fitz  # PyMuPDF
 import glob
 import pathlib
-import pandas as pd
+# import pandas as pd
+import polars as pl
 import re 
-from attendance_2024 import IRISH_NAME_REGEX
 pdf_payment = pathlib.Path(r"C:\Users\pglyn\PycharmProjects\dail_extractor\pdf_payments")
 #https://www.oireachtas.ie/en/publications/?q=standard%20allowance&date=&term=%2Fie%2Foireachtas%2Fhouse%2Fdail%2F34&fromDate=03%2F04%2F2026&toDate=03%2F04%2F2026
 
 # totally_exclude = ['Parliamentary', 'Standard', 'Allowance']
 # header_names = ['Name', 'TAA Band', 'Narrative', 'Date Paid', 'Amount']
-EXCLUDE_PLACEHOLDER = re.compile(r"^(PSA)")
+EXCLUDE_PLACEHOLDER = re.compile(r"^(Parliamentary Standard)")
 all_rows = []
 print('Starting to process payment PDFs...')
 for pdf in pdf_payment.glob("*.pdf"):
     # print(f"Processing payment file: {pdf}...")
     doc = fitz.open(pdf)  # Open the PDF document using PyMuPDF
     # print(f"Number of pages in {pdf}: {doc.page_count}")  # Debug: print the number of pages
+    print(f"Processing payment file: {pdf} with {doc.page_count} pages...")
     for page in doc:
         # print(f"Processing page {page.number} of {pdf}...")  # Debug: print the current page number
         # table = page.find_tables()
         for table in page.find_tables().tables:
-            # print(f"Processing table on page {page.number} of {pdf}...") 
-            if not EXCLUDE_PLACEHOLDER.search(table):
-                all_rows.extend(table.extract())
-            # print('DataFrame created from the table:')
-        
-# year = [2020, 2021, 2022, 2023, 2024, 2025, 2026]
-# month = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"]
+            all_rows.extend(table.extract()) 
 
-# construct_string = 
+cleaned_rows = []
+for row in all_rows:
+    if not EXCLUDE_PLACEHOLDER.search(str(row[0] or "")):
+        cleaned_rows.append(row)
+all_rows = cleaned_rows
+
+print(f"Total rows extracted: {len(all_rows)}")
+df = pl.DataFrame(all_rows, schema=['Name', 'TAA_Band', 'Narrative', 'Date_Paid', 'Amount'], orient='row')
+df = df.with_columns(pl.col('Name').str.splitn(by=' ', n=2
+                    ).alias('Name_Split'))
+df = df.with_columns(pl.col('Name_Split'
+                    ).struct.rename_fields(["Position", "Full_Name"])
+                    ).unnest("Name_Split").drop('Name')
+#TODO filter logic for Ceann Comhairle 
+#TODO filter logic for TDs who were elected after the payment date (e.g. payments made in 2024 should only be matched to TDs elected in 2024 or earlier)
+df = df.with_columns(
+        # NFD converts accented chars into base letter + combining accent mark to make it easier to join
+        pl.col('Full_Name')
+        .str.to_lowercase()
+        .str.replace_all(r"[\x27\u2019]", "")
+        .str.replace_all(r"[\u0300-\u036f]", "") # remove accents and fadas as it becomes too difficult to join names with special characters otherwise (e.g. O'Sullivan, Ó Súilleabháin)
+        .str.replace_all(r"[^a-z\s]", "")
+        .str.normalize("NFD").alias('join_key')
+        .str.replace_all(r"\s+", "")# Remove all whitespace
+        .str.extract_all(r".")    # Extract individual characters into a list       
+        .list.sort() # Sort the list of characters alphabetically (e.g. "OSuilleabhain" → "Oabhiillnsuu")
+        .list.join("") # Join the sorted characters back into a string
+        )
+df.write_csv('C:\\Users\\pglyn\\PycharmProjects\\dail_extractor\\members\\aggregated_payment_tables.csv')
+      
