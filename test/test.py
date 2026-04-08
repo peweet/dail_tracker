2a0403ce5d8d3181413cabf62060025b1aad4837
Patrick Glynn <p.glynn18@gmail.com>
2026-04-08 12:24:56 +0100

    added comments for test logic

--- test/test.py
+++ test/test.py
@@ -3,3 +3,11 @@
 # a high level of code quality and to ensure that our project is reliable and robust as it evolves over time.
 #TODO: write tests for the services and utility functions in the project, and set up a test suite that can be run automatically as part of our CI/CD pipeline. This will help us to maintain a high level of code quality and to ensure that our project is reliable and robust as it evolves over time. We can start by writing tests for the most critical functions and services in the project, and then expand our test coverage over time as we add more features and functionality to the project. We can also use tools like pytest-cov to measure our test coverage and to identify any areas of the code that are not being tested adequately.
 #Check also pydantic, dataclass, and data expectations for future testing and validation of data structures and types across the codebase, to ensure that we are working with the correct data formats and to catch any issues early on in the development process. This will help us to maintain a high level of code quality and to ensure that our project is reliable and robust as it evolves over time.
+
+#Take care to have the test folder mirror the productive folder to ensure ease of navigation and comprehension when writing and running tests, and to make it easier to identify which tests correspond to which services and utility functions in the project. This will help us to maintain a high level of organization and to ensure that our tests are easy to find and run as part of our development process. For example, we can have a directory structure like this:
+# test/
+#     attendance/
+#     payments/ 
+#     lobbying/
+#     utility/  
+        
